f03e59cd6595021565dc115f221ab86385b2ba48
badr <badr@lionsgeek.ma>
2023-12-27 00:31:50 +0100

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-var str = "Hello";
-if (str[0].toUpperCase() == str[0]) {
-    console.log('First character is upper case.');
-}
-
 let home;
 while (home !== 1) {
     home = prompt(`If you want to "Login" type 1 \nIf you want to "Signing up" type 2 \nIf you want to "Change password" type 3`);
@@ -10,21 +5,47 @@ while (home !== 1) {
     if (home == 1) {
         let name;
         let upperCase = [];
+        let number = [];
         let LastName;
-        
+
         //~ check The first letter should be capitalized
-        while (name !== upperCaseTemp) {
+        while (name !== upperCase) {
             name = prompt("Your FullName ?");
             let upperCaseTemp = name.charAt(0).toUpperCase();
             upperCase.push(upperCaseTemp);
-            
-            if (name == upperCaseTemp) {
-                LastName = prompt("chno knak lah?")
+            let numberTemp = Number.isFinite(name);
+            number.push(numberTemp);
+
+            if ((name[0].toUpperCase() == name[0]) && (typeof number !== 'number')) {
+                let email;
+                while (email !== "badr@gmail.com") {
+                    email = prompt("Your Email?");
+
+                    if (email == "badr@gmail.com") {
+                        alert("nice")
+                    } else {
+                        alert("error email")
+                    }
+
+                }
+
                 break;
-            }else {
-                alert("The first letter should be capitalized!");           
+            } else {
+                alert("The first letter should be capitalized!");
             }
         }
         break;
     }
 }
+
+let age;
+while ((isNaN(age)) && (`${age}`.length > 2) ) {
+    age  = prompt(`your age ?`)
+    if (isNaN(age) == isNaN(true) && (`${age}`.length < 3))  {
+        alert("nadiii!!")
+    }else {
+        alert("Enter only Number!!")
+    }
+}
+
+    
